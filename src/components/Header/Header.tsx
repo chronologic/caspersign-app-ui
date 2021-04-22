6883d3b8046d31f8a5389c34ac2a9c198e9fb8a0
AndreyDrozdov <ovalab@yandex.ru>
2021-04-22 15:02:39 +0200

    components

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,28 +1,45 @@
 import React from "react";
 import styled from "styled-components";
-import { Layout, Space } from "antd";
-
-import FlexSpacer from "../FlexSpacer";
+import { Layout, Menu, Dropdown } from "antd";
+import { DownOutlined } from "@ant-design/icons";
 
 function Header() {
+  const menu = (
+    <Menu>
+      <Menu.Item key="1">Logout</Menu.Item>
+    </Menu>
+  );
   return (
     <Layout.Header>
       <HeaderContent>
-        <Space>
-          <div />
-        </Space>
-        <FlexSpacer />
+        <Dropdown trigger={["click"]} overlay={menu}>
+          <LoginButton>
+            your@gmail.com <DownOutlined />
+          </LoginButton>
+        </Dropdown>
       </HeaderContent>
     </Layout.Header>
   );
 }
 
+const LoginButton = styled.div`
+  cursor: pointer;
+  text-align: right;
+  font-size: 16px;
+  line-height: 20px;
+  margin-left: auto;
+  margin-top: 20px;
+`;
+
 const HeaderContent = styled.div`
   display: flex;
-  flex-direction: row;
-  align-items: center;
-  height: 100%;
-  line-height: normal;
+  width: 100%;
+  max-width: 1170px;
+  padding-right: 15px;
+  padding-left: 15px;
+  margin-right: auto;
+  margin-left: auto;
+  text-align: center;
 `;
 
 export default Header;
