b1d278823b4d57e98ab4e771461380af17c446aa
Marcin <zolkiewski.marcin@gmail.com>
2021-07-13 16:47:35 +0200

    added hack for prettier

--- src/components/App/GlobalStyle.ts
+++ src/components/App/GlobalStyle.ts
@@ -2,14 +2,19 @@ import { createGlobalStyle } from "styled-components";
 
 import { skyblue, cerulean } from "../colors";
 
-const Style = createGlobalStyle`
+// weird hack to make prettier recognize css in createGlobalStyle and format it
+const styled = {
+  div: createGlobalStyle,
+};
+
+const Style = styled.div`
   * {
     box-sizing: border-box;
   }
 
   body {
     margin: 0;
-    font-family: "proxima-nova",sans-serif;
+    font-family: "proxima-nova", sans-serif;
     font-weight: 400;
     font-style: normal;
   }
@@ -28,7 +33,7 @@ const Style = createGlobalStyle`
     padding: 0 24px 12px;
     background: white;
     margin-bottom: 1rem;
-  } 
+  }
   .ant-card-bordered {
     margin-bottom: 20px;
   }
@@ -43,43 +48,41 @@ const Style = createGlobalStyle`
     margin-left: auto;
   }
 
-
-
   /* Share icons */
 
   .resp-sharing-button__link,
   .resp-sharing-button__icon {
-    display: inline-block
+    display: inline-block;
   }
 
   .resp-sharing-button__link {
     text-decoration: none;
     color: #fff;
-    margin-left: 0.3em
+    margin-left: 0.3em;
   }
 
   .resp-sharing-button {
     transition: 25ms ease-out;
     padding: 0.25em 0.5em;
-    font-family: Helvetica Neue,Helvetica,Arial,sans-serif
+    font-family: Helvetica Neue, Helvetica, Arial, sans-serif;
   }
 
   .resp-sharing-button__icon svg {
     width: 1em;
     height: 1em;
     margin-right: 0.4em;
-    vertical-align: top
+    vertical-align: top;
   }
 
   .resp-sharing-button--small svg {
     margin: 0;
-    vertical-align: middle
+    vertical-align: middle;
   }
 
   .resp-sharing-button__icon--solid,
   .resp-sharing-button__icon--solidcircle {
     fill: #fff;
-    stroke: none
+    stroke: none;
   }
 
   .resp-sharing-button--facebook {
@@ -121,38 +124,48 @@ const Style = createGlobalStyle`
 
   .ant-upload.ant-upload-drag:not(.ant-upload-disabled):hover {
     border-color: ${skyblue};
-  }   
+  }
 
   .spinner {
     margin-top: 24px;
     text-align: center;
   }
 
-  .ant-pagination-jump-prev .ant-pagination-item-container 
-  .ant-pagination-item-link-icon, .ant-pagination-jump-next 
-  .ant-pagination-item-container .ant-pagination-item-link-icon {
+  .ant-pagination-jump-prev
+    .ant-pagination-item-container
+    .ant-pagination-item-link-icon,
+  .ant-pagination-jump-next
+    .ant-pagination-item-container
+    .ant-pagination-item-link-icon {
     color: ${cerulean};
   }
-  .ant-pagination-prev:focus .ant-pagination-item-link, 
-  .ant-pagination-next:focus .ant-pagination-item-link, 
-  .ant-pagination-prev:hover .ant-pagination-item-link, 
+  .ant-pagination-prev:focus .ant-pagination-item-link,
+  .ant-pagination-next:focus .ant-pagination-item-link,
+  .ant-pagination-prev:hover .ant-pagination-item-link,
   .ant-pagination-next:hover .ant-pagination-item-link {
     color: ${cerulean};
     border-color: ${cerulean};
   }
-  .ant-pagination-item:focus, .ant-pagination-item:hover {
+  .ant-pagination-item:focus,
+  .ant-pagination-item:hover {
     border-color: ${cerulean};
   }
-  .ant-pagination-item:focus a, .ant-pagination-item:hover a {
+  .ant-pagination-item:focus a,
+  .ant-pagination-item:hover a {
     color: ${cerulean};
   }
-  .ant-pagination-item-active, .ant-pagination-item-active a, 
-  .ant-pagination-item-active a:hover, 
-  .ant-pagination-item-active:focus, .ant-pagination-item-active:hover {
+  .ant-pagination-item-active,
+  .ant-pagination-item-active a,
+  .ant-pagination-item-active a:hover,
+  .ant-pagination-item-active:focus,
+  .ant-pagination-item-active:hover {
     border-color: ${cerulean};
     color: ${cerulean};
   }
-  .ant-steps-vertical > .ant-steps-item:not(:last-child) > .ant-steps-item-container > .ant-steps-item-tail {
+  .ant-steps-vertical
+    > .ant-steps-item:not(:last-child)
+    > .ant-steps-item-container
+    > .ant-steps-item-tail {
     left: 50%;
   }
   .ant-steps-vertical > .ant-steps-item .ant-steps-item-icon {
@@ -163,10 +176,16 @@ const Style = createGlobalStyle`
   .ant-steps-item-title {
     padding-right: 0;
   }
-  .ant-steps-item-wait > .ant-steps-item-container > .ant-steps-item-content > .ant-steps-item-description {
+  .ant-steps-item-wait
+    > .ant-steps-item-container
+    > .ant-steps-item-content
+    > .ant-steps-item-description {
     color: black;
   }
-  .ant-steps-item-wait > .ant-steps-item-container > .ant-steps-item-content > .ant-steps-item-title {
+  .ant-steps-item-wait
+    > .ant-steps-item-container
+    > .ant-steps-item-content
+    > .ant-steps-item-title {
     text-transform: uppercase;
   }
 
@@ -174,7 +193,6 @@ const Style = createGlobalStyle`
     background: ${skyblue};
     border-color: ${skyblue};
   }
-
 `;
 
 export default Style;
